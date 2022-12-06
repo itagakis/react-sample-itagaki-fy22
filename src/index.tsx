075173a1c30963a30dce8d6a3da13899e78e223f
itagakis <itagaki.s@opst.co.jp>
2022-12-06 21:02:42 +0900

    子ページとエラー表示を追加

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './styles/index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 import Howto from './pages/Howto/index';
+import Sample from './pages/Howto/sample/index'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
@@ -15,6 +16,12 @@ root.render(
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='howto' element={<Howto />} />
+        <Route path='howto/sample' element={<Sample />} />
+        <Route path='*' element={
+          <main>
+            <p>404えらー</p>
+          </main>
+        } />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/pages/Howto/sample/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sample = () => {
+    return (
+        <div>
+            <h1>Howto/sample</h1>
+            <h2>サンプルページ</h2>
+        </div>
+    )
+}
+
+export default Sample
