f51ee014e41fc5060cc2a6d582def0885584fbdd
itagakis <itagaki.s@opst.co.jp>
2022-12-06 21:08:03 +0900

    リンク追加

--- src/pages/Howto/index.tsx
+++ src/pages/Howto/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Howto = () => {
     return (
         <div>
             <h1>Howto/index.tsx</h1>
             <h2>Howtoページ</h2>
+            <Link to="./sample/">サンプルページに進む</Link>
         </div>
     )
 }
--- src/pages/Howto/sample/index.tsx
+++ src/pages/Howto/sample/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Sample = () => {
     return (
         <div>
             <h1>Howto/sample</h1>
             <h2>サンプルページ</h2>
+            <Link to="../howto/">Howtoページに戻る</Link>
         </div>
     )
 }
